0e9dc3504ebfec525c47a20987020361e9c266c4
Sergey Parilin <sergey.parilin@fxdd.com>
2013-10-07 23:16:35 +0400

    config start

---
+++ elpa/ergoemacs-mode-20130925.1705/ergoemacs-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "ergoemacs-mode" "20130925.1705" "Emacs mode based on common modern software interface and ergonomics. [github]" 'nil)
+
---
+++ init.el
@@ -0,0 +1,45 @@
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(load-theme 'solarized-light t)
+
+(setq show-paren-style 'expression)
+(show-paren-mode 2)
+
+(setq make-backup-files         nil) ; Don't want any backup files
+(setq auto-save-list-file-name  nil) ; Don't want any .saves files
+(setq auto-save-default         nil) ; Don't want any auto saving
+
+;; yalinum
+(require 'yalinum)
+(global-yalinum-mode t)
+
+;; ido config 
+(require 'ido)
+(id-mode t)
+(setq ido-enable-flex-matching t)
+
+;; Eroemacs config
+(require 'ergoemacs-mode)
+(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+(ergoemacs-mode 1)
+
+;; go-mode config
+(require 'go-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ergoemacs-mode-used "5.8.0")
+ '(ergoemacs-theme nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(yalinum-bar-face ((t (:background "#eee8d5" :foreground "#586e75")))))
